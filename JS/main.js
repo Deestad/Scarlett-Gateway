f46815c76f48b75b9a1a8020d297e617c1d1ed68
Deestad <kalliddel@gmail.com>
2023-11-27 21:16:34 -0300

    New buttons

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,30 @@
 console.log("Javascript initiated.")
 
+document.addEventListener("DOMContentLoaded", function(colors){
+    const buttons = document.getElementsByClassName('btn')
+    for (var i = 0; i < buttons.length; i++){
+        var roll = Math.floor(Math.random(1,5)*5) + 1;
+        console.log(roll)
+        if (roll == 1){
+            buttons[i].style.backgroundColor = "green";
+        }
+        else if (roll == 2){
+            buttons[i].style.backgroundColor = "blue";
+        }
+        else if (roll == 3){
+            buttons[i].style.backgroundColor = "red";
+        }
+        else if (roll == 4){
+            buttons[i].style.backgroundColor = "cornflowerblue";
+        }
+        else if (roll == 5){
+            buttons[i].style.backgroundColor = "darkslateblue";
+        }
+        
+    }
+}); 
+
+
 function ChangeLeftMenuButtonColors(id){
     const clicked_button = document.getElementById(`${id}`);
     const buttons = document.getElementsByClassName('menu-buttons');
@@ -10,10 +35,15 @@ function ChangeLeftMenuButtonColors(id){
 }
 
 function ToggleDropdown(id){
-    menu = document.getElementById(id)
-    const Item = document.querySelector('.dropdown');
+    var dropdowns = document.getElementsByClassName('dropdown')
+    var Item = document.querySelector(`#dropdown-${id}`);
+    
     if (Item.style.display == "none"){
+        for (var i = 0; i < dropdowns.length; i++){
+            dropdowns[i].style.display = "none";
+        }
         Item.style.display = "block";
+        
     }
     else if (Item.style.display == "block"){
         Item.style.display = "none";
